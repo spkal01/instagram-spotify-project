e73fed787da892962c6b94f63e7d667597b2d443
spkal01 <kalligeross@gmail.com>
2024-11-07 00:00:18 +0200

    Restructure the project: Add error handling and automatic execution

--- main.py
+++ main.py
@@ -2,15 +2,25 @@ from instagrapi import Client
 from instagrapi import AccountMixin
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+import time
+from config import *
 
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=APP_CLIENT_ID,
                                                client_secret=APP_CLIENT_SECRET,
                                                redirect_uri=APP_REDIRECT_URI,
                                                scope="user-read-currently-playing"))
-response = sp.currently_playing()
-track = response['item']['name']
-artists = " and ".join([artist['name'] for artist in response['item']['artists']])
-bio = "Now playing: " + track + " by " + artists + " ♪"
 cl = Client()
 cl.login(ACCOUNT_USERNAME, ACCOUNT_PASSWORD, True)
-cl.account_set_biography(bio)
+def execute():
+    response = sp.currently_playing()
+    if response == None:
+        return
+    else:
+        track = response['item']['name']
+        artists = " and ".join([artist['name'] for artist in response['item']['artists']])
+        bio = "Now playing: " + track + " by " + artists + " ♪"
+        cl.account_set_biography(bio)
+
+while True:
+    execute()
+    time.sleep(300)
